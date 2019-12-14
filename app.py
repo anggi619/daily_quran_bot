eb695d51dd0b8c72c18d8c0c88e242e8d2689b75
ydhnwb <yudhanewbie@gmail.com>
2019-12-15 01:30:10 +0700

    changed english quran to indonesia quran

--- app.py
+++ app.py
@@ -6,23 +6,26 @@ from media import Media
 md = Media()
 tw = Twitter()
 quran = Quran()
+surahs = quran.fetch_surah_data()
+
 def start():
     while True:
         try:
             md.download_image()
-            surah_and_ayah = quran.get_random_ayat()
-            print("Media has been downloaded!")
-            print(surah_and_ayah["surah_name"])
-            md.process_image(surah_and_ayah["ayah"], surah_and_ayah["surah_name"], surah_and_ayah["ayah_index"])
+            data = quran.get_ayah()
+            surah_name = surahs[data["index_surah"]-1]["name"]
+            print("Surah name is "+str(surah_name))
+            ayah_text = data["ayah"]["text"]
+            ayah_index = data["ayah"]["index"]
+            print("Ayah ke "+str(ayah_index)+ " and isi "+ str(ayah_text))
+            md.process_image(ayah_text, surah_name, ayah_index)
             tw.post_tweet_with_media()
-            print("Sleeping...")
             time.sleep(14400)
         except Exception as ex:
             print("Exception!")
             print(ex)
             time.sleep(90)
             pass
-
 
 
 if __name__ == "__main__":
--- quran.py
+++ quran.py
@@ -1,23 +1,27 @@
 import requests
-import _json
 import random
 
 class Quran:
-    def get_random_ayat(self):
-        surah_index = random.randrange(1, 114)
-        surah_req = requests.get("http://api.alquran.cloud/v1/surah/"+str(surah_index)+"/en.asad")
-        surah_data = surah_req.json()
-        num_of_ayah = surah_data["data"]["numberOfAyahs"]
-        choosed_ayah = random.randrange(1, num_of_ayah)
+    def fetch_surah_data(self):
+        url = requests.get("https://al-quran-8d642.firebaseio.com/data.json")
+        datas = url.json()
+        surahs = []
+        for item in datas:
+            surah_detail = {'index': item['nomor'], 'name': item['nama']}
+            surahs.append(surah_detail)
+        return surahs
+
+    def get_ayah(self):
+        choosed_surah = random.randrange(1,114)
+        print("Choosed surah "+ str(choosed_surah))
+        url = requests.get("https://al-quran-8d642.firebaseio.com/surat/"+str(choosed_surah)+".json")
+        datas = url.json()
         ayahs = []
-        for item in surah_data["data"]["ayahs"]:
-            surah_detail = {"index": None, "text": None}
-            surah_detail['index'] = item['numberInSurah']
-            surah_detail['text'] = item['text']
-            ayahs.append(surah_detail)
+        for item in datas:
+            ayah_detail = {'index':item['nomor'], 'text':item['id']}
+            ayahs.append(ayah_detail)
 
-        print("Loading surah "+ surah_data["data"]["englishName"] + " ayah number " + str(choosed_ayah))
-        print(ayahs[choosed_ayah]["text"])
-        surah_and_ayah = {"surah_name":surah_data["data"]["englishName"], "ayah":ayahs[choosed_ayah]["text"], "ayah_index":str(choosed_ayah)}
-        return  surah_and_ayah
-
+        choosed_ayah = random.randrange(len(ayahs))
+        print("Choosed ayah "+str(choosed_ayah))
+        data = {'index_surah':choosed_surah, 'ayah': ayahs[choosed_ayah]}
+        return data
